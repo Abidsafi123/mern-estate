8f97fb6b0706a2bcccdc9196927567cb01c1d2df
= <=>
2025-08-23 05:20:58 -0700

    create authRoute and authcontroller

---
+++ server/controller/user_Auth.js
@@ -0,0 +1,38 @@
+import userModel from "../model/user.js"
+
+import bcrypt from "bcrypt"
+
+export const register = async(req,res)=>{
+    try {
+        const{username,email,password} = req.body
+        // hashed the password
+        const existingUser = await  userModel.findOne({email})
+        if(existingUser){
+            res.status(400).json({
+                success:false,
+                message:"User already exist!"
+            })
+            
+        } 
+        const hashedPassword =  await bcrypt.hash(password,10)
+        const newUser =  new userModel({
+            username,
+            email,
+            password:hashedPassword
+
+        })
+        res.status(201).json({
+            success:true,
+            message:"User created successfully!",
+            user:newUser
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:'Internal server error',
+            error:error.message
+        })
+        
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import connection from "./config/db.js";
+import userRoute from "./routes/auth_route.js"
+import router from "./routes/auth_route.js";
 const app = express();
+app.use(express.json())
+app.use('/api',userRoute)
 connection();
+
+
 
 const port = 3000;
 app.listen(port, () => {
