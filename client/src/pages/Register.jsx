40c35717c9ecf09882ad60bd5d5e21bc173bc81c
= <=>
2025-08-25 05:12:14 -0700

    created sign up pages frontend and backend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,37 +1,88 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+ 
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // start loading
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/register",
+        formData
+      );
+
+      if (res.data.success) {
+        alert(res.data.message || "User Registered Successfully!");
+        navigate('/login')
+        setFormData({ username: "", email: "", password: "" });
+       
+      } else {
+        alert(res.data.message || "User Registration Failed!");
+      }
+    } catch (error) {
+      console.log("error", error);
+      alert(error.response?.data?.message || "Something went wrong!");
+    } finally {
+      setLoading(false); // stop loading after request finishes
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
           type="text"
           placeholder="username"
+          value={formData.username}
+          onChange={handleChange}
           className="border p-3 rounded-lg focus:outline-none "
           id="username"
         />
         <input
           type="email"
           placeholder="email"
+          value={formData.email}
           className="border p-3 rounded-lg focus:outline-none "
+          onChange={handleChange}
           id="email"
         />
         <input
           type="password"
           placeholder="password"
-          className="border p-3 rounded-lg focus:ouline-none "
+          value={formData.password}
+          className="border p-3 rounded-lg focus:outline-none "
+          onChange={handleChange}
           id="password"
         />
-        <button  className="bg-slate-700 text-white p-3 rounded-lg font-semibold uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          type="submit"
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg font-semibold uppercase hover:opacity-95 disabled:opacity-80 flex justify-center items-center"
+        >
+          {loading ? "Registering..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={'/login'} >
-        <span className="text-blue-700">Login</span>
+        <Link to={"/login"}>
+          <span className="text-blue-700">Login</span>
         </Link>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -2,21 +2,14 @@ import express from "express";
 import connection from "./config/db.js";
 import userRoute from "./routes/auth_route.js";
 import router from "./routes/auth_route.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.use("/api", userRoute);
 connection();
 
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = statusCode.message || "Internal server error";
-//   return res.status(statusCode.json({
-//     success:false,
-//     message,
-//     statusCode
-
-//   }))
-// });
 const port = 3000;
 app.listen(port, () => {
   console.log(`app is running on http://localhost:${port}`);
