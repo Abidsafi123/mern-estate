908d27a1500c7f720c89b0d416946cc1f649980f
= <=>
2025-08-25 03:23:31 -0700

    created complete signup page UI

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
  import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
+ import react from '@vitejs/plugin-react-swc'
+ export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),
   ],
 })
--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import connection from "./config/db.js";
-import userRoute from "./routes/auth_route.js"
+import userRoute from "./routes/auth_route.js";
 import router from "./routes/auth_route.js";
 const app = express();
-app.use(express.json())
-app.use('/api',userRoute)
+app.use(express.json());
+app.use("/api", userRoute);
 connection();
 
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = statusCode.message || "Internal server error";
+//   return res.status(statusCode.json({
+//     success:false,
+//     message,
+//     statusCode
 
-
+//   }))
+// });
 const port = 3000;
 app.listen(port, () => {
   console.log(`app is running on http://localhost:${port}`);
